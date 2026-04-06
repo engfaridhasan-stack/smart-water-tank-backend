c3e4846561e5994d3adc516b919b69bc3fd248f1
MD Farid Hasan <eng.faridhasan@gmail.com>
2026-04-07 02:34:07 +0600

    Updated apps.py for MQTT and fixed CSRF issues

--- water_tank/apps.py
+++ water_tank/apps.py
@@ -1,14 +1,17 @@
 from django.apps import AppConfig
 import os
+import threading
 
 class WaterTankConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'water_tank'
 
     def ready(self):
-        # জ্যাঙ্গো যাতে ডাবল থ্রেড তৈরি না করে তার জন্য একটি চেক
-        # RUN_MAIN চেক করে নিশ্চিত হওয়া যায় যে এটি মেইন প্রসেস কি না
-        if os.environ.get('RUN_MAIN') == 'true':
+        # লোকাল রিলোডার চেক (RUN_MAIN) অথবা রেন্ডার চেক (DAPHNE_RUN)
+        # যাতে ডাবল থ্রেড তৈরি না হয়
+        if os.environ.get('RUN_MAIN') == 'true' or os.environ.get('DAPHNE_RUN') == 'true' or not os.environ.get('DEBUG'):
             from . import mqtt_handler
             print("[SYSTEM] Initializing MQTT Connection...")
-            mqtt_handler.start_mqtt()
+            
+            # একটি সেফ থ্রেডে স্টার্ট করা যাতে জ্যাঙ্গোর মেইন প্রসেস আটকে না যায়
+            threading.Thread(target=mqtt_handler.start_mqtt, daemon=True).start()
